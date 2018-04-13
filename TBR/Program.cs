276b1c34a3336be2f0fd970dc7fbf86dbb09013c
davek@northbendeng.com <davek@northbendeng.com>
2018-04-12 17:58:45 -0700

    Added paginated list view. Supports all cursor navigation keys.

--- TBR/Program.cs
+++ TBR/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace TBR
 {
     class Program
     {
+        enum PageKey { Next, Previous, Home, End, Quit, None }
+
         static void Main(string[] args)
         {
             Console.WriteLine();
@@ -17,18 +20,160 @@ namespace TBR
             }
 
             TweetDataClient tdc = new TweetDataClient();
-            var results = tdc.GetItemsFromUrl(argValidator.Url, argValidator.StartDate, argValidator.EndDate, argValidator.Verbose);
-            Console.WriteLine("\n======================================");
-            Console.WriteLine($"Query from: {argValidator.StartDate} to {argValidator.EndDate}");
-            Console.WriteLine($"Records Found: {results.Count()}");
-            Console.WriteLine("======================================");
-            foreach (var result in results)
+            IEnumerable<Tweet> queryResult = null;
+            try
             {
-                Console.WriteLine($"{result.Id}   {result.Stamp}\n{result.Text}");
+                queryResult = tdc.GetItemsFromUrl(argValidator.Url, argValidator.StartDate, argValidator.EndDate,
+                    argValidator.Verbose);
             }
+            catch
+            {
+                Console.WriteLine("\n**Query Error - Cannot query the remote host\n");
+            }
+            
+            if (queryResult != null && queryResult.Count() > 0)
+            {
+                List<Tweet> results = queryResult.ToList();
+                // Set start index for page.
+                int startIndex = -1;
+                int endIndex = 0;
+                
+                // Show page
+                PageKey key = PageKey.Home;
+                do
+                {
+                    bool updateScreen = false;
+                    switch (key)
+                    {
+                        case PageKey.End:
+                            if (endIndex != results.Count)
+                            {
+                                endIndex = results.Count;
+                                startIndex = endIndex - argValidator.PageSize;
+                                if (startIndex < 0)
+                                {
+                                    startIndex = 0;
+                                }
+                                updateScreen = true;
+                            }
+                            break;
+                        case PageKey.Next:
+                            if (endIndex != results.Count)
+                            {
+                                if (startIndex + argValidator.PageSize < results.Count)
+                                {
+                                    startIndex += argValidator.PageSize;
+                                    endIndex += argValidator.PageSize;
+                                    updateScreen = true;
+                                }
+                            }
+                            break;
+                        case PageKey.Home:
+                            if (startIndex != 0)
+                            {
+                                Console.Clear();
+                                startIndex = 0;
+                                endIndex = startIndex +
+                                           ((argValidator.PageSize == 0) ? results.Count : argValidator.PageSize);
+                                updateScreen = true;
+                            }
+                            break;
+                        case PageKey.Previous:
+                            if (startIndex != 0)
+                            {
+                                startIndex -= argValidator.PageSize;
+                                if (startIndex < 0) startIndex = 0;
+                                endIndex = startIndex + argValidator.PageSize;
+                                updateScreen = true;
+                            }
+                            break;
+                        default:
+                            updateScreen = false;
+                            break;
+                    }
 
-            Console.Write("\nHit any key...");
-            Console.ReadKey();
+                    for (int i = startIndex; i < endIndex && i < results.Count() && updateScreen; i++)
+                    {
+                        Console.WriteLine($"{results[i].Id}   {results[i].Stamp}\n{results[i].Text}");
+                        Console.WriteLine("-------------------------------------------------------------------------");
+                    }
+
+                    if (argValidator.PageSize != 0)
+                    {
+                        if (updateScreen)
+                        {
+                            int endNum = (endIndex > results.Count) ? results.Count : endIndex;
+                            Console.WriteLine(
+                            $"\nRecords {startIndex + 1} - {endNum} of {results.Count}  < - previous  next - >\n");
+                        }
+                        key = CheckKey(Console.ReadKey());
+                    }
+                    else
+                    {
+                        key = PageKey.Quit;
+                    }
+                } while (key != PageKey.Quit);
+                Console.WriteLine("\n======================================");
+                Console.WriteLine($"Query from: {argValidator.StartDate} to {argValidator.EndDate}");
+                Console.WriteLine($"Records Found: {queryResult.Count()}");
+                Console.WriteLine("======================================");
+            }
+            else
+            {
+                Console.WriteLine("\n======================================");
+                Console.WriteLine($"Query from: {argValidator.StartDate} to {argValidator.EndDate}");
+                Console.WriteLine($"Records Found: 0");
+                Console.WriteLine("======================================");
+            }
+        }
+
+        private static PageKey CheckKey(ConsoleKeyInfo keyInfo)
+        {
+            Console.Write('\b');
+            switch (keyInfo.Key)
+            {
+                // Valid "Next Page" keys
+                case ConsoleKey.Spacebar:
+                    return PageKey.Next;
+                case ConsoleKey.Enter:
+                    return PageKey.Next;
+                case ConsoleKey.RightArrow:
+                    return PageKey.Next;
+                case ConsoleKey.DownArrow:
+                    return PageKey.Next;
+                case ConsoleKey.PageDown:
+                    return PageKey.Next;
+                case ConsoleKey.OemPeriod:
+                    return PageKey.Next;
+                case ConsoleKey.N:
+                    return PageKey.Next;
+                case ConsoleKey.End:
+                    return PageKey.End;
+                // Valid "Previous Page" keys
+                case ConsoleKey.LeftArrow:
+                    return PageKey.Previous;
+                case ConsoleKey.OemComma:
+                    return PageKey.Previous;
+                case ConsoleKey.B:
+                    return PageKey.Previous;
+                case ConsoleKey.P:
+                    return PageKey.Previous;
+                case ConsoleKey.UpArrow:
+                    return PageKey.Previous;
+                case ConsoleKey.PageUp:
+                    return PageKey.Previous;
+                case ConsoleKey.Home:
+                    return PageKey.Home;
+                // Exit keys
+                case ConsoleKey.X:
+                    return PageKey.Quit;
+                case ConsoleKey.Q:
+                    return PageKey.Quit;
+                default:
+                    Console.Write("\b \b");
+                    break;
+            }
+            return PageKey.None;
         }
 
         static void ShowHelp()
--- TBR/TweetDataClient.cs
+++ TBR/TweetDataClient.cs
@@ -87,22 +87,28 @@ namespace TBR
                 }
 
                 tweets = await GetTweetsAsync(uri.PathAndQuery);
-
-                // Check to see if the Tweet API's 100 record limit was reached.
-                if (!MaxRecordsExceeded(tweets))
+                if (tweets != null)
                 {
-                    allTweets.AddRange(tweets);
-                    doneQuerying = endDate == maxDate;
-                    if (!doneQuerying)
+                    // Check to see if the Tweet API's 100 record limit was reached.
+                    if (!MaxRecordsExceeded(tweets))
                     {
-                        startDate = IncrementDate(startDate, maxDate);
-                        endDate = IncrementDate(endDate, maxDate);
+                        allTweets.AddRange(tweets);
+                        doneQuerying = endDate == maxDate;
+                        if (!doneQuerying)
+                        {
+                            startDate = IncrementDate(startDate, maxDate);
+                            endDate = IncrementDate(endDate, maxDate);
+                        }
+                    }
+                    else
+                    {
+                        // Adjust endDate based on new _maxTimespan and resubmit the query
+                        endDate = IncrementDate(startDate, maxDate);
                     }
                 }
                 else
                 {
-                    // Adjust endDate based on new _maxTimespan and resubmit the query
-                    endDate = IncrementDate(startDate, maxDate);
+                    doneQuerying = true;
                 }
             }
 
